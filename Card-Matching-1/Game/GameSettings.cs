771892b6537cc0a8b57396bbb7333796f5096b01
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-09 01:09:55 +0900

    refactor : 카드 스킨, 난이도, 모드를 저장하기 위한 구조체 추가

---
+++ Card-Matching-1/Game/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿struct GameSettings {
+    private CardDisplayStyle _skin;
+    private Difficulty _diff;
+    private GameMode _mode;
+
+    public CardDisplayStyle Skin { 
+        get => _skin; 
+        set => _skin = value; 
+    }
+    public Difficulty Diff { 
+        get => _diff; 
+        set => _diff = value; 
+    }
+    public GameMode Mode { 
+        get => _mode; 
+        set => _mode = value; 
+    }
+}
