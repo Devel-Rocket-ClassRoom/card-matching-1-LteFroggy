7cd490e57c63375f1163177d0d9b307d23cae03d
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-09 01:10:02 +0900

    refactor : 수정된 코드 반영하여 Main 수정

--- Card-Matching-1/Program.cs
+++ Card-Matching-1/Program.cs
@@ -2,5 +2,17 @@ using System;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-GameManager gm = new GameManager();
-do { } while (gm.PlayGame());
+do {
+    GameSettings settings = new GameSettings();
+    Menu.ShowMainMenu();
+    settings.Mode = Menu.GetGameMode();
+    settings.Skin = Menu.GetCardDisplayStyle();
+    settings.Diff = Menu.SelectDifficulty();
+
+    Board board = BoardFactory.Create(settings);
+    GameModeBase gameMode = GameModeFactory.Create(settings);
+    GameManager gameManager = new GameManager(gameMode, settings, board);
+
+    gameManager.PlayGame();
+
+} while (Menu.AskReplay());
