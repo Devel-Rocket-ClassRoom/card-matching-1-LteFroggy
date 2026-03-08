64d4916db3a4583b0869577f9a653e97abebe864
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-09 01:03:19 +0900

    refactor : 보드 내부에서 카드 생성 로직 제거

---
+++ Card-Matching-1/Board/Board.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+internal class Board {
+    private ICard[] _cards;
+    private int[] _shuffledIdx;
+
+    private BoardState _boardState;
+
+    private int _colSize = 4;
+    private int _boardSize;
+
+    private int _firstFlippedIdx;
+    private int _secondFlippedIdx;
+
+    public BoardState BoardState => _boardState;
+    public int ColSize => _colSize;
+    public int BoardSize => _boardSize;
+    public int RowSize => _boardSize / _colSize;
+
+    public Board(ICard[] cards, int boardSize) { 
+        _cards = cards;
+        _boardSize = boardSize;
+        BoardShuffler.shuffle(_boardSize, _colSize, out _shuffledIdx);
+    }
+
+    /// <summary>
+    /// 보드판 상태 출력용 함수. 미리보기라면 모두 오픈한 상태로 공개
+    /// </summary>
+    /// <param name="isPreview">미리보기 여부</param>
+    public void PrintBoard(bool isPreview = false) {
+        Console.Write($"    1열 2열 3열 4열");
+        for (int i = 0; i < _boardSize; i++) {
+            // colsize마다 줄바꿈
+            if (i % _colSize == 0) { 
+                Console.Write($"\n{(i / _colSize) + 1}행  ");
+            }
+            Console.ForegroundColor = _cards[_shuffledIdx[i]].GetColor(isPreview);
+            Console.Write($"{_cards[_shuffledIdx[i]].GetPrintValue(isPreview)}");
+            Console.ResetColor();
+        }
+    }
+
+    /// <summary>
+    /// 카드 한 장 뒤집기
+    /// </summary>
+    /// <param name="row">행</param>
+    /// <param name="col">열</param>
+    public void FlipCard(int row, int col) {
+        int idx = row * _colSize + col;
+
+        // 이미 뒤집힌 카드를 다시 뒤집으려 하는 경우
+        if (_cards[_shuffledIdx[idx]].IsOpened) {
+            throw new InvalidOperationException("이미 뒤집힌 카드입니다.");
+        } 
+        // 첫 카드를 뒤집는 상태
+        else if (_boardState == BoardState.WaitingFirst) {
+            _firstFlippedIdx = _shuffledIdx[idx];
+            _cards[_firstFlippedIdx].IsOpened= true;
+            _boardState = BoardState.WaitingSecond;
+        } 
+        // 두 번째 카드를 뒤집는 상태
+        else if (_boardState == BoardState.WaitingSecond) {
+            _secondFlippedIdx = _shuffledIdx[idx];
+            if (_secondFlippedIdx == _firstFlippedIdx) { throw new InvalidOperationException("같은 카드를 다시 뒤집을 수 없습니다."); }
+            _cards[_secondFlippedIdx].IsOpened = true;
+            _boardState = BoardState.Matching;
+        }
+    }
+
+    /// <summary>
+    /// 두 카드를 뒤집은 상태에서, 두 카드가 동일한지 체크
+    /// </summary>
+    public bool MatchCard() {
+        if (_boardState != BoardState.Matching) {
+            throw new InvalidOperationException("아직 두 카드를 뒤집지 않았습니다.");
+        }
+
+        _boardState = BoardState.WaitingFirst;
+
+        // 두 값이 같다면, 두 값의 match 상태를 변경
+        if (_cards[_firstFlippedIdx].Value == _cards[_secondFlippedIdx].Value) {
+            _cards[_firstFlippedIdx].IsMatched = true;
+            _cards[_secondFlippedIdx].IsMatched = true;
+            return true;
+        }
+        // 다르다면, 오픈을 false 로 변경
+        else {
+            _cards[_firstFlippedIdx].IsOpened = false;
+            _cards[_secondFlippedIdx].IsOpened = false;
+            return false;
+        }
+    }
+}
