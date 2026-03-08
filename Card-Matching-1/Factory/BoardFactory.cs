58b1257bf172e5735bc7133e7ea44d397dc1b305
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-09 01:08:42 +0900

    refactor : 객체 생성 Factory클래스로 분리

---
+++ Card-Matching-1/Factory/BoardFactory.cs
@@ -0,0 +1,13 @@
+﻿static class BoardFactory {
+    public static Board Create(GameSettings settings) {
+        int size;
+        // 난이도에 따라 카드 수 결정
+        size = settings.Diff switch {
+                Difficulty.Easy => 8,
+                Difficulty.Normal => 16,
+                Difficulty.Hard => 24,
+        };
+
+        return new Board(CardFactory.Create(settings.Skin, size), size);
+    }
+}
---
+++ Card-Matching-1/Factory/CardFactory.cs
@@ -0,0 +1,25 @@
+﻿static class CardFactory {
+    public static ICard[] Create(CardDisplayStyle skin, int size) {
+        ICard[] cards = new ICard[size];
+        
+        switch (skin) {
+            case CardDisplayStyle.Number:
+                for (int i = 0; i < size; i++) {
+                    cards[i] = new NumberCard(i / 2 + 1);
+                }
+                break;
+            case CardDisplayStyle.Alphabet:
+                for (int i = 0; i < size; i++) {
+                    cards[i] = new AlphabetCard(i / 2 + 1);
+                }
+                break;
+            case CardDisplayStyle.Sign:
+                for (int i = 0; i < size; i++) {
+                    cards[i] = new SignCard(i / 2  + 1);
+                }
+                break;
+        }
+
+        return cards;
+    }
+}
